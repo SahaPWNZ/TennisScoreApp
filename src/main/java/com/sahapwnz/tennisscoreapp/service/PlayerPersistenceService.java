6998ac651bb95d737889f766b91ed15e1f74298a
вашÐSahaPWNZ <007saha@inbox.ru>
2024-08-09 12:14:48 +0300

    11

--- src/main/java/com/sahapwnz/tennisscoreapp/service/PlayerPersistenceService.java
+++ src/main/java/com/sahapwnz/tennisscoreapp/service/PlayerPersistenceService.java
@@ -1,4 +1,12 @@
 package com.sahapwnz.tennisscoreapp.service;
 
+import com.sahapwnz.tennisscoreapp.dao.PlayerDAO;
+import com.sahapwnz.tennisscoreapp.entity.Player;
+
 public class PlayerPersistenceService {
+//    private final PlayerDAO playerDAO = new PlayerDAO();
+//
+//    public Player findOrSave(PlayerRequestDTO playerDTO) {
+//        return playerDAO.
+//    }
 }
